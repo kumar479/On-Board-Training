4770c8e6fe9c51d561c304da2eaacd6b551c1ea5
kumar <munikumarkmk123@gmail.com>
2023-01-28 12:23:12 +0530

    Added API mapping for say_hello()

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+import flask
+
 def say_hello():
-    print("Hello World..!")
-say_hello()
+    print('Hello!')
+
+
+
+
+app = flask.Flask(__name__)
+
+@app.route('/api/say_hello', methods=['GET'])
+def say_hello_api():
+    return say_hello()
